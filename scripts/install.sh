982963eacf015a65911480952882ea438d73edeb
test <test@test.es>
2019-12-13 10:08:39 +0100

    Removed sudo as I believe it is not working

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,10 +5,10 @@ curl -O https://rpm.nodesource.com/setup_10.x
 bash setup_10.x
 yum install -y nodejs
 # Install vue
-sudo npm install -g vue-cli
+npm install -g vue-cli
 # Create project folder
-sudo mkdir -p /usr/public_html
-sudo cd /usr/public_html
+mkdir -p /usr/public_html
+cd /usr/public_html
 #sudo yum install -y expect
 #vue init webpack myvue-project
 #cd myvue-project
