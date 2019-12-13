d60cf7515214c544e7abd48f6d92146e30f5c806
test <test@test.es>
2019-12-13 10:04:33 +0100

    Modified script to properly download install file

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # Install node.js
 cd /tmp
-curl -sL https://rpm.nodesource.com/setup_10.x
-sudo bash setup_10.x
-sudo yum install -y nodejs
+curl -O https://rpm.nodesource.com/setup_10.x
+bash setup_10.x
+yum install -y nodejs
 # Install vue
 sudo npm install -g vue-cli
 # Create project folder
-mkdir -p $HOME/public_html
-cd $HOME/public_html
+sudo mkdir -p /usr/public_html
+sudo cd /usr/public_html
 #sudo yum install -y expect
 #vue init webpack myvue-project
 #cd myvue-project
 #npm run build
 # Install nginx
-sudo yum install -y epel-release
-sudo yum install -y nginx
-sudo systemctl start nginx
+yum install -y epel-release
+yum install -y nginx
+systemctl start nginx
